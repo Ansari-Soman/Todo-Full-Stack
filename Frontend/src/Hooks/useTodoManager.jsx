83254c1ac30907573629dd98299bf9ec38ef2ed6
Ansari Soman <ansarisoman4077@gmail.com>
2025-12-20 17:38:39 +0530

    Normalized todo hook

--- Frontend/src/Hooks/useTodoManager.jsx
+++ Frontend/src/Hooks/useTodoManager.jsx
@@ -3,71 +3,49 @@ import toast from "react-hot-toast";
 import { API_PATH } from "../Utils/apiPath";
 import axiosInstance from "../Utils/axiosInstance";
 import { TodoContext } from "../Context/TodoContext";
+import { handleRequest } from "../Utils/handleRequest";
 
 const useTodoManager = () => {
   const { setTodoList } = useContext(TodoContext);
 
   // Add one todo
-  const addTodo = async (todoName, date) => {
+  const addTodo = handleRequest(async (todoName, date) => {
     if (!todoName.trim() || !date) {
       return { success: false, error: "Name and date are required." };
     }
-    try {
-      const response = await axiosInstance.post(API_PATH.TODO.ADD_TODO, {
-        todoName,
-        date,
-      });
-      if (response.success) {
-        getAllTodos();
-        return { success: true, message: response.message };
-      }
-    } catch (error) {
-      return { success: false, error: error.message };
-    }
-  };
+    const response = await axiosInstance.post(API_PATH.TODO.ADD_TODO, {
+      todoName,
+      date,
+    });
+    getAllTodos();
+    return { success: true, message: response.message };
+  });
 
   // Get All Todos
-  const getAllTodos = async () => {
-    try {
-      const response = await axiosInstance.get(API_PATH.TODO.GET_ALL_TODO);
-      if (response.success && Array.isArray(response.todos)) {
-        setTodoList(response.todos);
-        return;
-      }
-      setTodoList([]);
-    } catch (error) {
-      return { success: false, error: error.message };
+  const getAllTodos = handleRequest(async () => {
+    const response = await axiosInstance.get(API_PATH.TODO.GET_ALL_TODO);
+    if (Array.isArray(response.todos)) {
+      setTodoList(response.todos);
+      return;
     }
-  };
+    setTodoList([]);
+  });
 
   // Delete One todo
-  const deleteTodo = async (id) => {
-    try {
-      const response = await axiosInstance.delete(
-        API_PATH.TODO.DELETE_TODO(id)
-      );
-      if (response.success) {
-        getAllTodos();
-
-        return { success: true, message: response.message };
-      }
-    } catch (error) {
-      return { success: false, error: error.message };
-    }
-  };
+  const deleteTodo = handleRequest(async (id) => {
+    const response = await axiosInstance.delete(API_PATH.TODO.DELETE_TODO(id));
+    getAllTodos();
+    return { success: true, message: response.message };
+  });
 
   // Update todo
-  const updateTodo = async (id, completed) => {
-    try {
-      const response = await axiosInstance.put(API_PATH.TODO.UPDATE_TODO(id), {
-        completed,
-      });
-      getAllTodos();
-      return { success: true, message: response.message };
-    } catch (error) {
-      return { success: false, error: error.message };
-    }
-  };
+  const updateTodo = handleRequest(async (id, completed) => {
+    const response = await axiosInstance.put(API_PATH.TODO.UPDATE_TODO(id), {
+      completed,
+    });
+    getAllTodos();
+    return { success: true, message: response.message };
+  });
 
   return {
     getAllTodos,
